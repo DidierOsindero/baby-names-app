cf648617fb4362be75754fa3ddbe8851a778fdb2
Didier Osindero <didierosindero@gmail.com>
2022-11-04 08:40:13 +0000

    Added App.css and imported to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { MainContent } from "./components/MainContent";
+import "./App.css";
 
 function App(): JSX.Element {
   return (
+  <div className="App">
   <MainContent />
+  </div>
   );
 }
 
