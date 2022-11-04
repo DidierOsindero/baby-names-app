de25191c15c4817eda7fb5ed28ccefb21523dba9
Didier Osindero <didierosindero@gmail.com>
2022-11-04 11:47:03 +0000

    Add helpful comment to BabyNames

--- src/components/BabyNames.tsx
+++ src/components/BabyNames.tsx
@@ -9,11 +9,12 @@ interface nameDataProps {
     name: string,
     sex: string
 }
-
+//props.jsonFile[index] => gives you specific name object
 export const BabyNames = (props: BabyNamesProps): JSX.Element => {
     return (
         <>
             <BabyName id={1} name="Jane" sex="f"/>
+            <p>{props.jsonFile[50].name}</p>
         </>
     );
 }
