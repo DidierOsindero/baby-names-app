7e81906b2dcc236b7ab27cbcc2c19015f63903fd
Didier Osindero <didierosindero@gmail.com>
2022-11-04 08:18:33 +0000

    Created BabyName Props

--- src/components/BabyName.tsx
+++ src/components/BabyName.tsx
@@ -1,3 +1,10 @@
-export const BabyName = (): JSX.Element => {
+interface BabyNameProps {
+    id: number,
+    name: string,
+    sex: string
+}
+
+
+export const BabyName = ({id, name, sex}: BabyNameProps): JSX.Element => {
     return (<></>);
 }
